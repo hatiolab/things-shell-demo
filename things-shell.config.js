444da7f85c5218530d185f5095d3dc2f8257adbc
Hearty Oh <heartyoh@hatiolab.com>
2018-03-05 23:31:36 +0900

    1.0.0

--- things-shell.config.js
+++ things-shell.config.js
@@ -1,22 +1,21 @@
-var templates = [{
-  name: 'ABC', /* 다국어 키 표현을 어떻게.. */
-  description: '...', /* 다국어 키 표현을 어떻게.. */
-  group: 'shape', /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
-  icon: '../', /* 또는, Object */
-  template: {
-    type: 'ABC',
-    model: {
-      type: 'abc',
-      width: 100,
-      height: 100,
-      fillStyle: 'red',
-      abc: 'ABC'
-    }
-  }
-}];
-
 import editors from './src/editors';
 import locales from './locales';
+
+import icon from './assets/no-image.png';
+
+var templates = [{
+  type: 'ABC',
+  description: 'abc component',
+  group: 'shape', /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
+  icon,
+  model: {
+    type: 'abc',
+    width: 100,
+    height: 100,
+    fillStyle: 'red',
+    abc: 'ABC'
+  }
+}];
 
 module.exports = {
   templates,
